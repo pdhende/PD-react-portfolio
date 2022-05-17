f74982812e67fd84bcb850ac083cf2ae32a75a22
pdhende <pdhendework@gmail.com>
2022-05-17 19:54:12 -0400

    Added navigation links

---
+++ src/components/MainContainer.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+import Header from './Header';
+import Navigation from './Navigation';
+import Project from './Project';
+
+function MainContainer() {
+    return (
+        <Navigation />
+    );
+}
+
+export default MainContainer;
---
+++ src/components/Navigation.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import '../styles/Navigation.css'
+
+function Navigation() {
+    return (
+        <nav className="navbar navbar-dark navbar-expand-lg w-100">
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                <li className="nav-item active">
+                    <a className="nav-link" href="#about-me">About Me </a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#Work">Work</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#contact-me">Contact Me</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="Priyanka_Dhende.pdf"  target="_blank">Resume</a>
+                </li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+export default Navigation;
